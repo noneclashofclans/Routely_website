a8bd24e9fc69d9cf69abf55ac04a345c8d743f8a
Rishit Mohanty <rishitmohanty3@gmail.com>
2025-10-25 19:48:31 +0530

    mongo_db changes

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,13 +1,17 @@
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose');
 
-const connectDB = async() => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Connection created: ${conn.connection.host}`);
-    }catch{
-        console.log('Error connecting database: ${error.message}');
-        process.exit(1)
+const connectDB = async () => {
+    try {
+        const dbName = process.env.MONGO_DB_NAME || 'routely_db';
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            dbName,
+            // you can add other options here if needed
+        });
+        console.log(`MongoDB connected: ${conn.connection.host}/${conn.connection.name}`);
+    } catch (error) {
+        console.error(`Error connecting to MongoDB: ${error.message}`);
+        process.exit(1);
     }
-}
+};
 
 module.exports = connectDB;
